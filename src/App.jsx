af21e6c356bca35e8505a1d742fb62b0f089f299
Marco Fiordi <marco.fiordi97@gmail.com>
2026-05-07 17:32:38 +0200

    generate card dinamiche con nome e immagine degli attori

--- src/App.jsx
+++ src/App.jsx
@@ -48,29 +48,21 @@ function App() {
         </section>
 
         <section>
-          <article className="actor-card">
-            <h2>Marco Fiordi</h2>
-            <img src="https://picsum.photos/300/300"
-              alt="Marco Fiordi"
-            />
-            <p>1997- Italian</p>
+          {actors.map((actor) => {
+            return (
+              <article className="actor-card" key={actor.id}>
+                <h2>{actor.name}</h2>
+                <img 
+                src={actor.image} 
+                alt={actor.name}
+                />
 
-            <p>
-              Marco Fiordi è uno studente del corso Boolean appassionato di videogiochi manga e anime,
-              ex pro player di call of duty e vice presidente di una asd che organizza tornei italiani ed europei
-            </p>
-
-            <p>
-              <strong>know for</strong> Il Signore dei Console.log, Fast & Fetchious,
-              AnsiaScript: Episodio I, e React contro tutti.
-            </p>
-
-            <p>
-              <strong>Awards:</strong> Campione italiano di ansia da prestazione,
-              vice campione di sollevatore di polemiche e lancio del coriandolo.
-              Il suo sogno: 'Avere un sogno'.
-            </p>
-          </article>
+                <p>
+                  {actor.birth_year} - {actor.nationality}
+                </p>
+              </article>
+            );
+          })}
         </section>
       </main>
     </>
