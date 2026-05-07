e9a05acacb2e52b54f5aafc37df0f8949c874a7f
Marco Fiordi <marco.fiordi97@gmail.com>
2026-05-07 16:44:11 +0200

    creata card statica attore

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,45 @@
 
 
 function App() {
-  return 
+  return (
+    <>
+      <header>
+        <p>Wonderful Cast</p>
+      </header>
+
+      <main>
+        <section>
+          <h1>Actors</h1>
+          <p>List of actors fetched from API</p>
+        </section>
+
+        <section>
+          <article>
+            <h2>Marco Fiordi</h2>
+            <img src=""
+              alt=""
+            />
+            <p>1997- Italian</p>
+
+            <p>
+              Marco Fiordi è uno studente del corso Boolean appassionato di videogiochi manga e anime,
+              ex pro player di call of duty e vice presidente di una asd che organizza tornei italiani ed europei
+            </p>
+
+            <p>
+              <strong>know for</strong> Il Signore dei Console.log, Fast & Fetchious,
+              AnsiaScript: Episodio I, e React contro tutti.
+            </p>
+
+            <p>
+              <strong>Awards:</strong> Campione italiano di ansia da prestazione,
+              vice campione di sollevatore di polemiche e lancio del coriandolo.
+              Il suo sogno: 'Avere un sogno'.
+            </p>
+          </article>
+        </section>
+      </main>
+    </>
+  )
 }
 export default App;
